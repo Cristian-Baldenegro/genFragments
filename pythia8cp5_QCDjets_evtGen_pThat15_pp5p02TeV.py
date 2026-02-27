bec6acab5534d9a59baa30eda4aa9db008b3a2ab
Cristian Baldenegro <Cristian-Baldenegro@users.noreply.github.com>
2026-02-27 13:45:31 -0500

    Update pythia8cp5_QCDjets_evtGen_pThat15_pp5p02TeV.py

--- pythia8cp5_QCDjets_evtGen_pThat15_pp5p02TeV.py
+++ pythia8cp5_QCDjets_evtGen_pThat15_pp5p02TeV.py
@@ -1,7 +1,7 @@
 import FWCore.ParameterSet.Config as cms
 
 from Configuration.Generator.Pythia8CommonSettings_cfi import *
-from Configuration.Generator.MCTunesRun3ECM13p6TeV.PythiaCP5Settings_cfi import *
+from Configuration.Generator.MCTunes2017.PythiaCP5Settings_cfi import *
 
 from GeneratorInterface.EvtGenInterface.EvtGenSetting_cff import *
 # If you want the default EvtGen decay settings too, you can also import:
